46730bf84b8c9ae4fd0fc013be2ba8f99fc9746d
Jonathan Widawski <jonathan.widawski@gmail.com>
2017-11-24 18:13:00 +0100

    ➕ New sheet binding

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ export default class App extends Component {
       <div className='container'>
         <ReactGoogleSheetConnector 
             apiKey='AIzaSyC-TuIFSYuF1LqDQpxm_olaCfl4qJLd-Q4'
-            spreadsheetId='1usDQD5lyawfnsOwxa4pAc568C3aIQRBsR2_XKSUMuQ4'
+            spreadsheetId='1gZr0cWDmMiDA5PJ9ztaVMxMu0vxnWjaIbLLDvsssyYE'
             spinner={ <div className="loading-spinner"/> } >
             <Content />
         </ReactGoogleSheetConnector>
